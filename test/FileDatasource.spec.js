9d607ca23a4d94f5cc1ae985f042c278d3ffe02e
Perry Mitchell <perry@perrymitchell.net>
2018-03-18 14:41:15 +0200

    Add tests for file datasource

---
+++ test/FileDatasource.spec.js
@@ -0,0 +1,78 @@
+const path = require("path");
+const fs = require("fs");
+const rimraf = require("rimraf").sync;
+const fileExists = require("file-exists");
+const { Archive, createCredentials } = require("buttercup");
+const FileDatasource = require("../source/FileDatasource.js");
+const TextDatasource = require("../source/TextDatasource.js");
+
+describe("FileDatasource", function() {
+    it("instantiates without error", function() {
+        expect(() => {
+            new FileDatasource("./test.bcup");
+        }).to.not.throw();
+    });
+
+    describe("load", function() {
+        beforeEach(function() {
+            this.archive = new Archive();
+            this.path = path.resolve(__dirname, "./test.bcup");
+            this.datasource = new FileDatasource(this.path);
+            const tds = new TextDatasource();
+            return tds
+                .save(this.archive, createCredentials.fromPassword("test"))
+                .then(encryptedArchive => {
+                    fs.writeFileSync(this.path, encryptedArchive);
+                });
+        });
+
+        afterEach(function() {
+            rimraf(this.path);
+        });
+
+        it("reads from a file", function() {
+            return expect(this.datasource.load(createCredentials.fromPassword("test"))).to
+                .eventually.be.fulfilled;
+        });
+
+        it("loads an archive", function() {
+            return this.datasource.load(createCredentials.fromPassword("test")).then(archive => {
+                expect(archive).to.be.an.instanceof(Archive);
+            });
+        });
+    });
+
+    describe("save", function() {
+        beforeEach(function() {
+            this.archive = new Archive();
+            this.path = path.resolve(__dirname, "./test.bcup");
+            this.datasource = new FileDatasource(this.path);
+            rimraf(this.path);
+        });
+
+        afterEach(function() {
+            rimraf(this.path);
+        });
+
+        it("writes to a file", function() {
+            return this.datasource
+                .save(this.archive, createCredentials.fromPassword("test"))
+                .then(() => {
+                    return expect(fileExists(this.path)).to.eventually.be.true;
+                });
+        });
+
+        it("writes an archive", function() {
+            return this.datasource
+                .save(this.archive, createCredentials.fromPassword("test"))
+                .then(() => {
+                    const contents = fs.readFileSync(this.path, "utf8");
+                    const tds = new TextDatasource(contents);
+                    return tds.load(createCredentials.fromPassword("test"));
+                })
+                .then(archive => {
+                    expect(archive).to.be.an.instanceof(Archive);
+                });
+        });
+    });
+});
