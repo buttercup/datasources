01dacdc09fc2701f62ea6c503de74f28901fd95f
Perry Mitchell <perry@perrymitchell.net>
2018-03-18 12:52:38 +0200

    Add webdav/owncloud datasources

---
+++ source/WebDAVDatasource.js
@@ -0,0 +1,121 @@
+const createWebDAVClient = require("webdav");
+const TextDatasource = require("./TextDatasource.js");
+const { registerDatasource } = require("./DatasourceAdapter.js");
+
+/**
+ * WebDAV datasource for reading and writing remote archives
+ * @augments TextDatasource
+ */
+class WebDAVDatasource extends TextDatasource {
+    /**
+     * Constructor for the datasource
+     * @param {string} endpoint URL for the WebDAV service (without resource path)
+     * @param {string} webDAVPath Resource path on the WebDAV service
+     * @param {Credentials=} credentials Credentials (username/password) for the WebDAV service
+     */
+    constructor(endpoint, webDAVPath, credentials) {
+        super();
+        this._endpoint = endpoint;
+        this._path = webDAVPath;
+        this._client = credentials ?
+            createWebDAVClient(endpoint, credentials.username, credentials.password) :
+            createWebDAVClient(endpoint);
+    }
+
+    /**
+     * The WebDAV client instance
+     * @type {Object}
+     * @memberof WebDAVDatasource
+     */
+    get client() {
+        return this._client;
+    }
+
+    /**
+     * The remote WebDAV endpoint
+     * @type {String}
+     * @memberof WebDAVDatasource
+     */
+    get endpoint() {
+        return this._endpoint;
+    }
+
+    /**
+     * The remote archive path
+     * @type {String}
+     * @memberof WebDAVDatasource
+     */
+    get path() {
+        return this._path;
+    }
+
+    /**
+     * Load the archive from the datasource
+     * @param {Credentials} credentials The credentials for archive decryption
+     * @returns {Promise.<Archive>} A promise resolving with the archive
+     * @memberof WebDAVDatasource
+     */
+    load(credentials) {
+        return this.client
+            .getFileContents(this.path, "text")
+            .then(content => {
+                this.setContent(content);
+                return super.load(credentials);
+            });
+    }
+
+    /**
+     * Save an archive to the WebDAV service
+     * @param {Archive} archive The archive to save
+     * @param {Credentials} credentials The credentials for encryption
+     * @returns {Promise} A promise resolving when the save is complete
+     * @memberof WebDAVDatasource
+     */
+    save(archive, credentials) {
+        return super
+            .save(archive, credentials)
+            .then(encrypted => this.client.putFileContents(this.path, encrypted));
+    }
+
+    /**
+     * Output the datasource as an object
+     * @returns {Object} An object describing the datasource
+     * @memberof WebDAVDatasource
+     */
+    toObject() {
+        return {
+            type: "webdav",
+            endpoint: this.endpoint,
+            path: this.path
+        };
+    }
+}
+
+/**
+ * Create an instance from an object
+ * @param {Object} obj The WebDAV info object
+ * @param {Credentials=} hostCredentials The server credentials
+ * @static
+ * @memberof WebDAVDatasource
+ */
+WebDAVDatasource.fromObject = function fromObject(obj, hostCredentials) {
+    if (obj.type === "webdav") {
+        return new WebDAVDatasource(obj.endpoint, obj.path, hostCredentials);
+    }
+    throw new Error(`Unknown or invalid type: ${obj.type}`);
+};
+
+/**
+ * Create an instance from a string
+ * @param {String} str The string representation of the datasource
+ * @param {Credentials=} hostCredentials The server credentials
+ * @static
+ * @memberof WebDAVDatasource
+ */
+WebDAVDatasource.fromString = function fromString(str, hostCredentials) {
+    return WebDAVDatasource.fromObject(JSON.parse(str), hostCredentials);
+};
+
+registerDatasource("webdav", WebDAVDatasource);
+
+module.exports = WebDAVDatasource;
